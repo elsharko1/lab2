fb49900265c3774c3e2cde0aab731b37b34dbf1f
elsharko1 <elsharko1@gmail.com>
2017-06-27 17:24:59 -0400

    first add

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int userNum = 0;
+        char contAns = ' ';
+
+
+        do {
+            System.out.println("Enter a number between 1 and 100: ");
+            userNum = scnr.nextInt();
+            if (userNum > 100) {
+                System.out.println("Please enter a number between 1 and 100:");
+            } else if (userNum % 2 == 1) {
+                System.out.println(userNum + "Odd");
+                // write your code here
+            } else if (userNum % 2 == 0 && userNum > 1 && userNum < 25) {
+                System.out.println(" Even and less than 25.");
+            } else if (userNum % 2 == 0 && userNum > 25 && userNum < 61) {
+                System.out.println("Even");
+            } else if (userNum % 2 == 0 && userNum > 60 && userNum < 101) {
+                System.out.println(userNum + " Even");
+            } else if (userNum % 2 == 1 && userNum > 60 && userNum < 101) {
+                System.out.println(userNum + " Odd and over 60");
+            }
+            System.out.println("continue y/n: ");
+            contAns = scnr.next().charAt(0);
+
+        } while (contAns == 'y');
+
+
+    }
+}
