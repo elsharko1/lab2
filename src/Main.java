3b99f5a9e8df057be4ce09f6f835d5c94fae0f02
elsharko1 <elsharko1@gmail.com>
2017-06-27 22:17:28 -0400

    changed a couple more things, not done yet, need comments

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,55 @@
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         int userNum = 0;
         char contAns = ' ';
+        String userName = "";
+        int userAge = 0;
+
+        System.out.println("Please enter your name: ");
+        userName = scnr.nextLine();
+
+        System.out.println("Hello " + userName);
+        System.out.println("");
 
 
+        System.out.println("What is your age " + userName);
+        scnr = new Scanner(System.in);
+        userAge = scnr.nextInt();
+        System.out.println("");
 
 
         do {
-            System.out.println("Enter a number between 1 and 100: ");
+            System.out.println("Hey " + userName + " enter a number between 1 and 100: ");
             userNum = scnr.nextInt();
             if (userNum >= 100) {
                 System.out.println("Please enter a number between 1 and 100:");
             } else if (userNum % 2 == 1 && userNum <= 59) {
-                System.out.println(userNum + " Odd");
+                System.out.println(userNum + " Odd\n");
+                System.out.println("Good choice that is an odd number and it is " + (Math.abs(userAge - userNum)) + " away from your age.\n");
                 // write your code here lets add more comments
             } else if (userNum % 2 == 0 && userNum > 1 && userNum < 25) {
-                System.out.println(" Even and less than 25.");
+                System.out.println(" Even and less than 25.\n");
+                System.out.println("Good choice that is an odd number and it is " + (Math.abs(userAge - userNum)) + " away from your age.\n");
             } else if (userNum % 2 == 0 && userNum > 25 && userNum < 61) {
-                System.out.println(" Even");
+                System.out.println(" Even\n");
+                System.out.println("Good choice that is an odd number and it is " + (Math.abs(userAge - userNum)) + " away from your age.\n");
             } else if (userNum % 2 == 0 && userNum > 60 && userNum < 101) {
-                System.out.println(userNum + " Even");
+                System.out.println(userNum + " Even\n");
+                System.out.println("Good choice that is an odd number and it is " + (Math.abs(userAge - userNum)) + " away from your age.\n");
             } else if (userNum % 2 == 1 && userNum > 60 && userNum < 101) {
-                System.out.println(userNum + " Odd and over 60");
+                System.out.println(userNum + " Odd and over 60\n");
+                System.out.println("Good choice that is an odd number and it is " + (Math.abs(userAge - userNum)) + " away from your age.\n");
             }
             System.out.println("continue y/n: ");
             contAns = scnr.next().charAt(0);
+            if (contAns == 'n') {
+                System.out.println("Good bye!!!");
+            }
 
         } while (contAns == 'y');
 
